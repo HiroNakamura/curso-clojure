4e684e450aa470793c7a28d980c9846a29b894ff
HIRONAKAMURA <carraro.ariel@gmail.com>
2022-01-02 10:34:57 -0600

    Se edito la extension del programa de cjl a clj

--- src/fundamentos/mapa.clj
+++ src/fundamentos/mapa.clj
@@ -1,6 +1,17 @@
 (ns mapa)
 
+(comment
+  Uso de mapas como registros
+)
+
 (def empleado {:nombre "Mariana" :edad 33 :salario 24000 :compania "INFORHOMEX"})
 (println "Mapa empleado: "empleado)
 (println "Nombre: "(empleado :nombre))
 (println "Edad: "(empleado :edad))
+
+;;
+(def tarot {:mago 1})
+
+
+
+
